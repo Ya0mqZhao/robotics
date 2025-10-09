e682e96833f20a970279345cefa15311eda1c5d6
Manqi <yao1mqz@gmail.com>
2025-10-09 14:56:41 -0700

    Update human.java

--- xrpdetection/human.java
+++ xrpdetection/human.java
@@ -77,19 +77,20 @@ public void autonomousPeriodic() {
     System.out.println(gyroValue); 
     
 if (step == 0) {
-      if (SensorValue < 0.4) {
+      if (sensorValue < 0.4) {
         step = 1;
       } else {
         m_drivetrain.arcadeDrive(1, 0);
       }
     } else if (step == 1) {
-      if (SensorValue > 0.6) {
+      if (sensorValue > 0.6) {
         step = 0;
       } else {
         m_drivetrain.arcadeDrive(0, 1);
       }
-    } else if (SensorValue > 5) {
-      m_drivetrain.arcadeDrive(0, 0);
+    } else if (sensorValue > 5) {
+      m_drivetrain.arcadeDrive(0, 0);}
+}
   /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {}
