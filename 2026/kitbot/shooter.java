eaca3902446f851dd3e08bd1d68c23983e56b508
Manqi <yao1mqz@gmail.com>
2026-01-23 08:32:29 -0800

    Implement Shooter class for motor control

---
+++ 2026/kitbot/shooter.java
@@ -0,0 +1,64 @@
+package frc.robot;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+public class Shooter {
+private final TalonFX shootMotor = new TalonFX(10, "canivore");  // Initializes the shootMotor
+private boolean isSpinUp = false;
+
+  // 
+  public Shooter() {
+    configMotor(shootMotor, false, 80.0); // Configures the motor with counterclockwise rotation positive and 80A current limit. 
+  }
+
+  //Turns on motor
+  public void spinUp() {
+    shootMotor.setControl(new VelocityVoltage(80.0).withEnableFOC(true)); // Sets the velocity of the motor in rotations per second.
+    isSpinUp = true;
+  }
+
+  //Turn off motor
+  public void spinDown() {
+    shootMotor.setControl(new VelocityVoltage(0.0).withEnableFOC(true)); // Sets the velocity of the motor in rotations per second.
+      isSpinUp = false;
+  }
+
+  //
+  public void updateDash() {
+    SmartDashboard.putBoolean("Shoot motor is on", isSpinUp);
+
+  }
+
+  //
+  private double getVelocity() {
+    return 0.0;
+  }
+  
+  //
+  private void configMotor() {
+
+  }
+  private void configMotor(TalonFX motor, boolean invert, double currentLimit) {
+  TalonFXConfiguration motorConfigs = new TalonFXConfiguration();
+
+  motorConfigs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+  motorConfigs.MotorOutput.Inverted = invert ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive;
+
+  // Current limit configuration.
+  motorConfigs.CurrentLimits.StatorCurrentLimitEnable = true;
+  motorConfigs.CurrentLimits.StatorCurrentLimit = currentLimit;
+
+  // VelocityVoltage closed-loop control configuration.
+  motorConfigs.Slot0.kP = 0.25; // Units: volts per 1 motor rotation per second of error.
+  motorConfigs.Slot0.kI = 0.5; // Units: volts per 1 motor rotation per second * 1 second of error.
+  motorConfigs.Slot0.kD = 0.0; // Units: volts per 1 motor rotation per second / 1 second of error.
+  motorConfigs.Slot0.kV = 0.12; // The amount of voltage required to create 1 motor rotation per second.
+  motorConfigs.Slot0.kS = 0.16; // The amount of voltage required to barely overcome static friction.
+
+  motor.getConfigurator().apply(motorConfigs, 0.03);
+}
+}
