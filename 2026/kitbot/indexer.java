a66568d7e244ac31c055201e1162add5509a043f
Manqi <yao1mqz@gmail.com>
2026-01-23 19:27:23 -0800

    task: (check)
    try: just use sensor to make sure evrything is passing through during shooting session
    organize code:
    sensor update (or cleanup)

--- 2026/kitbot/indexer.java
+++ 2026/kitbot/indexer.java
@@ -1,6 +1,9 @@
 package frc.robot;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.CANrangeConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.CANrange;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
@@ -12,13 +15,17 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Indexer {
   public enum Mode {FORWARD, JAM, IDLE}
   private final TalonFX IndexMotor = new TalonFX(0, "canivore");
+  private final CANrange indexSensor = new CANrange(0);
   private final Timer IndexTimer = new Timer();
+  private final StatusSignal<Boolean> fuelDetected;
   private Mode currState = Mode.IDLE;
   private final AnalogPotentiometer sensor = new AnalogPotentiometer(0, 5.0);
   
   public Indexer() {
     configMotor();
     IndexTimer.start();
+    configCANrange(indexSensor);
+    fuelDetected = indexSensor.getIsDetected();
   }
 
   public void init() {
@@ -28,17 +35,18 @@ public class Indexer {
   }
 
   public void periodic() {
-    if (currState == Mode.FORWARD && getSensor() && IndexTimer.hasElapsed(2.0)) {
+    if (getSensor() && IndexTimer.get() > 2) {//if sensor detects something and constantly detected for over 2 second execute mode.jam
       currState = Mode.JAM;
-      IndexTimer.reset();
     }
+      else{
+        IndexTimer.reset();
+      }
     
-    if (currState == Mode.JAM && IndexTimer.hasElapsed(10)) {
+    if (currState == Mode.JAM && !getSensor()) {//codition to get out of mode.jam 
       currState = Mode.IDLE;
       
     }
 
-    
     switch(currState) {
       case FORWARD:
         IndexMotor.setControl(new VelocityVoltage(-10.0).withEnableFOC(true));
@@ -66,18 +74,19 @@ public class Indexer {
     IndexTimer.reset();
   }
   public double getSensorTimer() {
-    return IndexTimer.get();
+    return 0.0;
   }
 
   public boolean getSensor() {
-    return sensor.get() > 2.5;
+    return fuelDetected.refresh().getValue();
   }
 
   public void updateDash() {
     SmartDashboard.putString("Indexer State", currState.toString());
     SmartDashboard.putBoolean("Indexer Sensor", getSensor());
-    SmartDashboard.putNumber("Indexer Sensor Value", sensor.get());
+    SmartDashboard.putBoolean("Fuel Detected", getSensor());
     SmartDashboard.putNumber("Indexer Sensor Timer", getSensorTimer());
+
   }
     
 
@@ -93,5 +102,13 @@ public class Indexer {
     motorConfigs.Slot0.kV = 0.12;
     motorConfigs.Slot0.kS = 0.1;
     IndexMotor.getConfigurator().apply(motorConfigs, 0.03);
+  }
+  private void configCANrange(CANrange sensor) {
+    CANrangeConfiguration sensorConfigs = new CANrangeConfiguration();
+
+    // Sets the proximity for how far away the fuel can be to get detected.
+    sensorConfigs.ProximityParams.ProximityThreshold = 0.4;
+
+    sensor.getConfigurator().apply(sensorConfigs, 0.03);
   }
 }
