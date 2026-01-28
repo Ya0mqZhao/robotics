3c296023ca3021b8fc81b4141135390de0bfea70
Manqi <yao1mqz@gmail.com>
2026-01-28 11:15:53 -0800

    Update indexer.java

--- 2026/kitbot/indexer.java
+++ 2026/kitbot/indexer.java
@@ -1,3 +1,4 @@
+
 package frc.robot;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.CANrangeConfiguration;
@@ -7,108 +8,134 @@ import com.ctre.phoenix6.hardware.CANrange;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.CANBus;
 
-import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Indexer {
   public enum Mode {FORWARD, JAM, IDLE}
-  private final TalonFX IndexMotor = new TalonFX(0, "canivore");
-  private final CANrange indexSensor = new CANrange(0);
-  private final Timer IndexTimer = new Timer();
-  private final StatusSignal<Boolean> fuelDetected;
+  private final CANBus canivore = new CANBus("canivore");
+  private final TalonFX IndexMotor = new TalonFX(0, canivore);
+  private final TalonFX shootMotor = new TalonFX(10, canivore);  // Initializes the shootMotor
+  private final CANrange ShooterSensor = new CANrange(11, canivore);
+  private final CANrange HopperSensor = new CANrange(12, canivore);
+  private final Timer ShooterTimer = new Timer();
+  private final Timer HopperTimer = new Timer();
+  private final Timer JamTimer = new Timer();
+  private final StatusSignal<Boolean> shooterFuelDetected;
+  private final StatusSignal<Boolean> hopperFuelDetected;
   private Mode currState = Mode.IDLE;
   private boolean isShooting = false;
   
+  // Initialize indexer: configure motor, start timer, configure sensor, and obtain fuelDetected data
   public Indexer() {
     configMotor();
-    IndexTimer.start();
-    configCANrange(indexSensor);
-    fuelDetected = indexSensor.getIsDetected();
+    ShooterTimer.start();
+    configCANrange(ShooterSensor);
+    configCANrange(HopperSensor);
+    shooterFuelDetected = ShooterSensor.getIsDetected();
+    hopperFuelDetected = HopperSensor.getIsDetected();
   }
 
+  // Runs code once at start: set current state to IDLE, stop shooting, stops motor, and reset the index timer
   public void init() {
     currState = Mode.IDLE;
     isShooting = false;
     IndexMotor.setControl(new VelocityVoltage(0.0).withEnableFOC(true));
-    IndexTimer.reset();
+    ShooterTimer.reset();
   }
 
+  // Runs code periodically: refresh the fuel sensor, run the shooting/jam state machine, and set motor outputs for each state
   public void periodic() {
-    fuelDetected.refresh();
-    
-    if (isShooting && currState == Mode.FORWARD) {
-      if (!getSensor() && IndexTimer.get() > 5.0) {
-        currState = Mode.JAM;
-        IndexTimer.reset();
-      } else if (getSensor()) {
-        IndexTimer.reset();
-      }
+    shooterFuelDetected.refresh();
+    hopperFuelDetected.refresh();
+    if (getShooterSensor()){
+     ShooterTimer.restart();
     }
-    
-    if (currState == Mode.JAM && IndexTimer.get() > 5.0) {
-      currState = Mode.IDLE;
-      isShooting = false;
+    if (getHopperSensor()){
+      HopperTimer.restart();
     }
-
     switch(currState) {
-      case FORWARD:
+      case FORWARD://just going forward
+        IndexMotor.setControl(new VelocityVoltage(10.0).withEnableFOC(true));
+        if(HopperTimer.get() > 3.0 ){
+          currState = Mode.IDLE;
+        }
+        else if(ShooterTimer.get() > 10.0 && HopperTimer.get() < 2.0 ){
+          currState = Mode.JAM;
+          JamTimer.restart();
+        }
+        break;
+      case JAM://execute when sensor is on false for 5 second
         IndexMotor.setControl(new VelocityVoltage(-10.0).withEnableFOC(true));
+        if (HopperTimer.get() >10.0 ) {
+          currState = Mode.IDLE;
+         }
+         else if (ShooterTimer.get() <1.0 && JamTimer.get() >10.0){
+          currState = Mode.FORWARD;
+         }
         break;
-      case JAM:
-        IndexMotor.setControl(new VelocityVoltage(20.0).withEnableFOC(true));
-        break;
-      case IDLE:
+      case IDLE://just stops
         IndexMotor.setControl(new VelocityVoltage(0.0).withEnableFOC(true));
         break;
     }
   }
 
+  // Marks the Indexer as running forward (not shooting) and resets the jam timer.
   public void start() {
     currState = Mode.FORWARD;
-    isShooting = false;
-    IndexTimer.reset();
+    isShooting = true;
+    ShooterTimer.reset();
   }
 
+  //Marks the indexer as idle, stops shooting, and reset the jam timer.
   public void stop() {
     currState = Mode.IDLE;
     isShooting = false;
     IndexMotor.setControl(new VelocityVoltage(0.0).withEnableFOC(true));
   }
 
+  // Marks the Indexer as running forward, enable shooting mode, and reset the jam timer.
   public void startShooting() {
     currState = Mode.FORWARD;
     isShooting = true;
-    IndexTimer.reset();
+    ShooterTimer.reset();
   }
   
+  // Mark the indexer as jammed, stop shooting, and reset the timer used for jam handling.
   public void jammed(){
     currState = Mode.JAM;
     isShooting = false;
-    IndexTimer.reset();
+    ShooterTimer.reset();
   }
 
+  // Return elapsed time (seconds) from IndexTimer since last reset
   public double getSensorTimer() {
-    return IndexTimer.get();
+    return ShooterTimer.get();
   }
 
-  public boolean getSensor() {
-    return fuelDetected.getValue();
+  // return a boolean if the sensor sense a fuel.
+  public boolean getShooterSensor() {
+    return shooterFuelDetected.getValue();
+  }
+  public boolean getHopperSensor() {
+    return hopperFuelDetected.getValue();
   }
 
+  // Publish indexer information (state, sensor, shooting flag, and timer) to SmartDashboard
   public void updateDash() {
     SmartDashboard.putString("Indexer State", currState.toString());
-    SmartDashboard.putBoolean("Indexer Sensor", getSensor());
-    SmartDashboard.putBoolean("Fuel Detected", getSensor());
+    SmartDashboard.putBoolean("Fuel Detected", getShooterSensor());
     SmartDashboard.putBoolean("Is Shooting", isShooting);
     SmartDashboard.putNumber("Indexer Sensor Timer", getSensorTimer());
   }
-    
+  
+  // Configs the motor settings and PID
   private void configMotor() {
     TalonFXConfiguration motorConfigs = new TalonFXConfiguration();
     motorConfigs.MotorOutput.NeutralMode = NeutralModeValue.Coast;
-    motorConfigs.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+    motorConfigs.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
     motorConfigs.CurrentLimits.StatorCurrentLimitEnable = true;
     motorConfigs.CurrentLimits.StatorCurrentLimit = 40.0;
     motorConfigs.Slot0.kP = 0.2;
@@ -119,9 +146,11 @@ public class Indexer {
     IndexMotor.getConfigurator().apply(motorConfigs, 0.03);
   }
   
+  // Configs the sensor settings 
   private void configCANrange(CANrange sensor) {
     CANrangeConfiguration sensorConfigs = new CANrangeConfiguration();
-    sensorConfigs.ProximityParams.ProximityThreshold = 0.4;
+    sensorConfigs.ProximityParams.ProximityThreshold = 0.45;
     sensor.getConfigurator().apply(sensorConfigs, 0.03);
   }
+  
 }
