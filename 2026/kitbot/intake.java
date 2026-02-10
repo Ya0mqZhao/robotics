2fe769c3f29f3a4b5f1c8119cdc4d374180d1f5e
Manqi <yao1mqz@gmail.com>
2026-02-10 15:07:48 -0800

    Refactor intake.java for improved functionality

--- 2026/kitbot/intake.java
+++ 2026/kitbot/intake.java
@@ -1,9 +1,10 @@
 package frc.robot;
-//remember to reference climber.java
+
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
@@ -25,16 +26,20 @@ public class Intake {
   private final TalonFX leftIntakeDeploy = new TalonFX(16, canivore);
   private final TalonFX leftIntake = new TalonFX(17, canivore);
   private final Timer intakeTimer = new Timer();
+  private boolean isHomed = false;
+  private boolean rightArmConfirmedUp = false;
+  private boolean leftArmConfirmedUp = false; 
   
   // Control requests
   private final PositionVoltage armPositionRequest = new PositionVoltage(0.0).withEnableFOC(true);
   private final VelocityVoltage rollerVelocityRequest = new VelocityVoltage(0.0).withEnableFOC(true);
+  private final VoltageOut voltageOutRequest = new VoltageOut(0.0);
 
   public Intake() {
-    configMotor(rightIntakeDeploy, true, 40.0, true);  // Arm motor
-    configMotor(rightIntake, false, 60.0, false);      // Roller motor
-    configMotor(leftIntakeDeploy, false, 40.0, true);  // Arm motor
-    configMotor(leftIntake, true, 60.0, false);        // Roller motor
+    configMotor(rightIntakeDeploy, true, 40.0, true);
+    configMotor(rightIntake, false, 60.0, false);
+    configMotor(leftIntakeDeploy, false, 40.0, true);
+    configMotor(leftIntake, true, 60.0, false);
     intakeTimer.start();
   }
 
@@ -44,60 +49,115 @@ public class Intake {
     desiredLeftRollerVelocity = 0.0;
     desiredRightArmPosition = 0.0;
     desiredRightRollerVelocity = 0.0;
+    isHomed = false;
+    rightArmConfirmedUp = false;
+    leftArmConfirmedUp = false;
     intakeTimer.reset();
   }
 
   public void periodic() {
     switch (currMode) {
       case HOME:
-        desiredLeftArmPosition = 0.0;
-        desiredLeftRollerVelocity = 0.0;
-        desiredRightArmPosition = 0.0;
-        desiredRightRollerVelocity = 0.0;
+        if (!isHomed) {
+          leftIntakeDeploy.setControl(voltageOutRequest.withOutput(-2.0));
+          rightIntakeDeploy.setControl(voltageOutRequest.withOutput(-2.0));
+          leftIntake.setControl(rollerVelocityRequest.withVelocity(0.0));
+          rightIntake.setControl(rollerVelocityRequest.withVelocity(0.0));
+          double leftVel = Math.abs(leftIntakeDeploy.getVelocity().getValueAsDouble());
+          double rightVel = Math.abs(rightIntakeDeploy.getVelocity().getValueAsDouble());
+          boolean leftStopped = leftVel < 0.05;
+          boolean rightStopped = rightVel < 0.05;
+          
+          if (leftStopped && rightStopped) {
+            if (intakeTimer.hasElapsed(1.0)) {
+              leftIntakeDeploy.setPosition(0.0);
+              rightIntakeDeploy.setPosition(0.0);
+              isHomed = true;
+              currMode = Mode.STOW;
+              rightArmConfirmedUp = false;
+              leftArmConfirmedUp = false;
+            }
+          } else {
+            intakeTimer.reset();
+          }
+        }
         break;
-
       case LEFT:
-        desiredLeftArmPosition = 2.0;
+        if (!rightArmConfirmedUp) {
+          desiredRightArmPosition = 0.0;
+          desiredLeftArmPosition = getLeftArmPosition();
+          if (Math.abs(getRightArmPosition() - 0.0) < 0.1) {
+            rightArmConfirmedUp = true;
+            intakeTimer.reset();
+          }
+        } else {
+          if (intakeTimer.hasElapsed(0.1)) {
+            desiredRightArmPosition = 0.0;
+            desiredLeftArmPosition = 2.0;
+          }
+        }
         desiredLeftRollerVelocity = 10.0;
-        desiredRightArmPosition = 0.0;
-        desiredRightRollerVelocity = 0.0;
+        desiredRightRollerVelocity = 0.0; 
         break;
-
+        
       case RIGHT:
-        desiredLeftArmPosition = 0.0;
+        if (!leftArmConfirmedUp) {
+          desiredLeftArmPosition = 0.0;
+          desiredRightArmPosition = getRightArmPosition(); 
+          if (Math.abs(getLeftArmPosition() - 0.0) < 0.1) {
+            leftArmConfirmedUp = true;
+            intakeTimer.reset(); 
+          }
+        } else {
+          if (intakeTimer.hasElapsed(0.1)) {
+            desiredLeftArmPosition = 0.0;
+            desiredRightArmPosition = 2.0;
+          }
+        }
         desiredLeftRollerVelocity = 0.0;
-        desiredRightArmPosition = 2.0;
         desiredRightRollerVelocity = 10.0;
         break;
-
+        
       case STOW:
-        desiredLeftArmPosition = 0.5;
+        desiredLeftArmPosition = 0.0;
+        desiredRightArmPosition = 0.0;
         desiredLeftRollerVelocity = 0.0;
-        desiredRightArmPosition = 0.5; 
         desiredRightRollerVelocity = 0.0;
         break;
     }
-    
-    // Apply desired positions/velocities
-    leftIntakeDeploy.setControl(armPositionRequest.withPosition(desiredLeftArmPosition));
-    leftIntake.setControl(rollerVelocityRequest.withVelocity(desiredLeftRollerVelocity));
-    rightIntakeDeploy.setControl(armPositionRequest.withPosition(desiredRightArmPosition));
-    rightIntake.setControl(rollerVelocityRequest.withVelocity(desiredRightRollerVelocity));
+    if (currMode != Mode.HOME) {
+      leftIntakeDeploy.setControl(armPositionRequest.withPosition(desiredLeftArmPosition));
+      leftIntake.setControl(rollerVelocityRequest.withVelocity(desiredLeftRollerVelocity));
+      rightIntakeDeploy.setControl(armPositionRequest.withPosition(desiredRightArmPosition));
+      rightIntake.setControl(rollerVelocityRequest.withVelocity(desiredRightRollerVelocity));
+    }
   }
 
   public void leftIntake() {
-    currMode = Mode.LEFT;
-    intakeTimer.reset();
+    if (isHomed) {
+      currMode = Mode.LEFT;
+      rightArmConfirmedUp = false;
+      leftArmConfirmedUp = false;
+      intakeTimer.reset();
+    }
   }
 
   public void rightIntake() {
-    currMode = Mode.RIGHT;
-    intakeTimer.reset();
+    if (isHomed) {
+      currMode = Mode.RIGHT;
+      rightArmConfirmedUp = false;
+      leftArmConfirmedUp = false;
+      intakeTimer.reset();
+    }
   }
 
   public void stowIntake() {
-    currMode = Mode.STOW;
-    intakeTimer.reset();
+    if (isHomed) {
+      currMode = Mode.STOW;
+      rightArmConfirmedUp = false;
+      leftArmConfirmedUp = false;
+      intakeTimer.reset();
+    }
   }
 
   public Mode getMode() {
@@ -121,7 +181,7 @@ public class Intake {
   }
 
   public boolean leftArmInPosition() {
-    return Math.abs(getLeftArmPosition() - desiredLeftArmPosition) < 0.5;
+    return Math.abs(getLeftArmPosition() - desiredLeftArmPosition) < 0.1;
   }
 
   public boolean leftRollerAtSpeed() {
@@ -145,7 +205,7 @@ public class Intake {
   }
   
   public boolean rightArmInPosition() {
-    return Math.abs(getRightArmPosition() - desiredRightArmPosition) < 0.5;
+    return Math.abs(getRightArmPosition() - desiredRightArmPosition) < 0.1;
   }
 
   public boolean rightRollerAtSpeed() {
@@ -157,9 +217,17 @@ public class Intake {
            rightArmInPosition() && rightRollerAtSpeed();
   }
 
+  public boolean getIsHomed() {
+    return isHomed;
+  }
+
   public void updateDash() {
     SmartDashboard.putString("Intake Mode", currMode.toString());
     SmartDashboard.putBoolean("Intake Ready", isReady());
+    SmartDashboard.putBoolean("Intake Homed", isHomed);
+    SmartDashboard.putBoolean("Right Arm Confirmed Up", rightArmConfirmedUp);
+    SmartDashboard.putBoolean("Left Arm Confirmed Up", leftArmConfirmedUp);
+    SmartDashboard.putNumber("Intake Timer", intakeTimer.get());
     SmartDashboard.putNumber("Left Arm Position", getLeftArmPosition());
     SmartDashboard.putNumber("Left Arm Desired", getLeftArmDesiredPosition());
     SmartDashboard.putNumber("Left Roller Vel", getLeftRollerVelocity());
@@ -178,12 +246,10 @@ public class Intake {
     motorConfigs.CurrentLimits.StatorCurrentLimit = currentLimit;
     
     if (isArmMotor) {
-      // Position control for arm motors
       motorConfigs.Slot0.kP = 2.0;
       motorConfigs.Slot0.kI = 0.5;
       motorConfigs.Slot0.kD = 0.0;
     } else {
-      // Velocity control for roller motors
       motorConfigs.Slot0.kP = 0.2;
       motorConfigs.Slot0.kI = 0.1;
       motorConfigs.Slot0.kD = 0.0;
